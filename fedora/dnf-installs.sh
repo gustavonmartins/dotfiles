97a9b8c72d5172e7c29201d683cc739a854ac709
Gustavo Nunes Martins <martins.yo@gmail.com>
2025-07-28 13:08:08 +0200

    fedora packages, redshift

--- fedora/dnf-installs.sh
+++ fedora/dnf-installs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo dnf install awesome evince flatpak fuse-libs gajim gnucash geany htop keepassxc libreoffice python3-pip obfs4 qbittorrent redshift-gtk rofi thunderbird tor torbrowser-launcher skim syncthing             
+sudo dnf install awesome evince flatpak fuse-libs gajim gnucash geany htop keepassxc libreoffice python3-pip obfs4 qbittorrent qemu redshift-gtk rofi thunderbird tor torbrowser-launcher skim syncthing             
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.telegram.desktop
